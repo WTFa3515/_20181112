449561247d7d231e3e795afa476c7a7bf864eb5e
WTFa3515 <sinba840720@gmail.com>
2018-11-16 21:25:36 +0800

    use "try-with-resources" to reduce finally{} and fix catch() "e->ex"

--- src/readfile/FileUtil.java
+++ src/readfile/FileUtil.java
@@ -7,24 +7,19 @@ import java.util.Scanner;
 public class FileUtil {
 	public static String readFile(String name) throws FileNotFoundException {
 		StringBuilder builder = new StringBuilder();
-		Scanner scanner = null;
+		
 		//delete try catch & use (abstract way) throws FileNotFoundException 
-			try {
-				scanner = new Scanner(new FileInputStream(name));
+			try(Scanner scanner = new Scanner(new FileInputStream(name)))
+			{//After JDK7, add a new grammar "try-with-resources" to reduce finally
 				while(scanner.hasNext()) {
 					builder.append(scanner.nextLine());
 					builder.append("\n");
 				}
-			} catch (FileNotFoundException e) {
+			} catch (FileNotFoundException ex) {//e->ex throw Exception before close file 
 				// TODO Auto-generated catch block
-				e.printStackTrace();
-				throw e;	//because when not in cmd interface, U need ues throw to show the exception
-			}finally {
-				if (scanner != null) {
-					scanner.close();
-				}
-			}
-		
+				ex.printStackTrace();
+				throw ex;	//because when not in cmd interface, U need ues throw to show the exception
+			}		
 		return builder.toString();
 	}
 }
