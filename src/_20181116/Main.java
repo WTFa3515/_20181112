89d68aed524bfdcc7861458cab3e3d79ecc271bd
WTFa3515 <sinba840720@gmail.com>
2018-11-16 21:43:34 +0800

    fix doSome add doOther implement AutoCloseable and use both on "try-with-resources" at same time

--- src/_20181116/Main.java
+++ src/_20181116/Main.java
@@ -6,8 +6,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		try(Resource res = new Resource()){
+		try(ResourceSome res = new ResourceSome();
+			ResourceOther other = new ResourceOther()){
 			res.doSome();
+			other.doOther();
 		}catch(Exception ex) {
 			ex.printStackTrace();
 		}
@@ -15,7 +17,7 @@ public class Main {
 
 }
 
-class Resource implements AutoCloseable{
+class ResourceSome implements AutoCloseable{
 	void doSome() {
 		System.out.println("do something");
 		Scanner scanner = new Scanner(System.in);
@@ -24,6 +26,19 @@ class Resource implements AutoCloseable{
 	}
 	@Override
 	public void close() throws Exception{
-		System.out.println("resource had closed");
+		System.out.println("resource \"Some\" had closed");
+	}
+}
+
+class ResourceOther implements AutoCloseable{
+	void doOther() {
+		System.out.println("do something");
+		Scanner scanner = new Scanner(System.in);
+		System.out.println(scanner.nextInt());
+		//can input not number to try the exception and close()
+	}
+	@Override
+	public void close() throws Exception{
+		System.out.println("resource \"Other\"had closed");
 	}
 }
