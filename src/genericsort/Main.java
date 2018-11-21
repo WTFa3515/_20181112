ea07e691a34bfa268f9fdf41f12d88add4212b8f
WTFa3515 <sinba840720@gmail.com>
2018-11-21 20:44:21 +0800

    modify Main.java remove ReversedStringOder change to used lambda to call comparator                 <without implement>

--- src/genericsort/Main.java
+++ src/genericsort/Main.java
@@ -1,18 +1,17 @@
 package genericsort;
 import java.util.*;
 
-class ReversedStringOrder implements Comparator<String>{
-	@Override
-	public int compare(String s1, String s2) {
-		// TODO Auto-generated method stub
-		return -s1.compareTo(s2);
-	}
-}
 
 public class Main{
 	public static void main(String[] args) {
 		String[] words = {"B", "X", "A", "M", "F", "W", "O"};
-		Arrays.sort(words, new ReversedStringOrder());
+		Arrays.sort(words, new Comparator<String>(){
+			@Override
+			public int compare(String s1, String s2) {
+				// TODO Auto-generated method stub
+				return -s1.compareTo(s2);
+			}
+		});
 		for(String word: words) {
 			System.out.println(word);
 		}
