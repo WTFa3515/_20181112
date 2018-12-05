bd709f0af51f8280387dc54f02314701d451987c
WTFa3515 <sinba840720@gmail.com>
2018-12-05 22:00:42 +0800

    fix out of max print

--- src/ClassTest/Factory_Mart_Custom.java
+++ src/ClassTest/Factory_Mart_Custom.java
@@ -3,12 +3,13 @@ import static java.lang.System.out;
 import java.util.concurrent.TimeUnit;
 
 public class Factory_Mart_Custom {
-	static int warehouse = 50;
+	static int warehouse = 25;
 	final static int maxrestock = 10;
-	//¯Ê³f Out of Stock
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//max warehouse is 50 
 		while(warehouse <= 50 && warehouse > 0) {
+			out.println("===================");
 			int randint = (int) (Math.random()*20);
 			out.printf("Now warehouse have %d goods\n", warehouse);
 			try {
@@ -16,16 +17,28 @@ public class Factory_Mart_Custom {
 				TimeUnit.SECONDS.sleep(1);
 				if(warehouse < 10) {
 					warehouse += maxrestock;
+				}else{
+					warehouse += maxrestock * (1- warehouse/50);
 				}
 				out.printf("%d customers take out %d goods\n", customers, customers);
-				out.printf("After shopping  warehouse have %d goods\n", warehouse);
 				warehouse -= customers;
+				out.printf("After shopping  warehouse have %d goods\n", warehouse);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
-		out.print("sell out, break credit");
+		if (warehouse > 50) {
+			out.println("warehouse is break");	
+		}else {
+			out.println("Out of Stock, break credit");	
+		}
 	}
-
+	
+	static void result(String outp){
+		out.println("Out of Stock, break credit");
+	}
+	static void result(int outp){
+		out.println("warehouse is break");
+	}
 }
